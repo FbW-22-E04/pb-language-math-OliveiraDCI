aaea2d493dd71d8517fe21e8a24aa3fa247f6e81
OliveiraDCI <oliveira.sein.dci@gmail.com>
2022-07-05 15:37:25 +0200

    Exercise concluded.

---
+++ solution.js
@@ -0,0 +1,22 @@
+// 1.a
+console.log(`1.a. The lowest number is ${Math.min(-1, 4)}`);
+
+// 1.b
+console.log(`1.b. The highest number is ${Math.max(-1, 4)}`);
+
+// 2.a
+console.log(`2.a. Round up result: ${Math.ceil(3321.32321)}`);
+console.log(`2.a. Round up result: ${Math.ceil(326.76)}`);
+console.log(`2.a. Round up result: ${Math.ceil(76788.7)}`);
+console.log(`2.a. Round up result: ${Math.ceil(-9.78)}`);
+console.log(`2.a. Round up result: ${Math.ceil(43.342)}`);
+
+// 2.b
+console.log(`2.b. Round down result: ${Math.floor(3321.32321)}`);
+console.log(`2.b. Round down result: ${Math.floor(326.76)}`);
+console.log(`2.b. Round down result: ${Math.floor(76788.7)}`);
+console.log(`2.b. Round down result: ${Math.floor(-9.78)}`);
+console.log(`2.b. Round down result: ${Math.floor(28.329)}`);
+
+// 3
+console.log(`3. Dice Roll: ${Math.ceil(Math.random() * 6)}`);
